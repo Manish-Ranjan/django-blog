2edffb1161abc117a9bab775646f0de9c1c0a746
Manish Ranjan <manishranjandbg@gmail.com>
2020-04-15 20:35:25 +0530

    create ,update and delete post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import(
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from blog.models import Post
 
 
@@ -9,6 +16,60 @@ def home(request, *args, **kwargs):
         "posts": Post.objects.all()
     }
     return render(request, 'home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering = ['date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "post_form.html"
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = "post_form.html"
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "post_confirm_delete.html"
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+
+            return False
 
 
 def about(request, *args, **kwargs):
