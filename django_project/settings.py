24e966f6b30fd11389dc1411af96afa58ddb4cef
Manish Ranjan <manishranjandbg@gmail.com>
2020-04-16 12:55:56 +0530

    user model

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'phone_field',
     'crispy_forms',
+    'PIL',
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
--- users/models.py
+++ users/models.py
@@ -11,9 +11,8 @@ class profile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        super().save(force_insert=force_insert, force_update=force_update,
-                     using=using, update_fields=update_fields)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
