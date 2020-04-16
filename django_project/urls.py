8f91e17fee3f75cc55357e9881b9f77835f63346
Manish Ranjan <manishranjandbg@gmail.com>
2020-04-16 12:46:20 +0530

    Email and password reset

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,7 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 from django.urls import path, include
 from users.views import register, profile
 from django.conf import settings
@@ -26,6 +33,14 @@ urlpatterns = [
     path('profile/', profile, name="profile"),
     path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('password-reset/', PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name="password_reset"),
+    path('password-reset/done/', PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name="password_reset_confirm"),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name="password_reset_complete"),
     path('', include("blog.urls")),
 ]
 if settings.DEBUG:
